d55084a2c82b184bd3ad88e074ec75317e57a024
domijassin <dominique.jassin@ac-lyon.fr>
2014-03-20 16:42:27 +0100

    Update modif_oscar_deploie.sh
    changement des trois lignes ok

--- modif_oscar_deploie.sh
+++ modif_oscar_deploie.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 #### modification oscar déploie pour oscar 3.2.2
 #### version 1
-sed -i.BAK "/%programfiles%/a \if exist \"%programfiles%\\\Wpkg-GP\" echo oui  > %windir%\\\oscar_deploie\\\oui_wpkg" oscar_deploie_numero_poste.bat
+sed -i.BAK "/%programfiles%/a \if exist \"%programfiles%\\\Wpkg-GP\" echo oui  > %windir%\\\oscar_deploie\\\oui_wpkg" /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
+sed -i.BAK "/oui_wpkg/a \if exist \"%programfiles%\\\Wpkg-GP\\\uninstall.exe\" call \"%programfiles%\\\Wpkg-GP\\\unistall.exe\" \/S" /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
+sed -i.BAK "/oscar_samba_domaine.reg/a \copy \\\\%ip_scribe%\\\admin\\\perso\\\oscar_deploie\\\oscar_var.txt \\\\%ipscribe%\\\sysprep\\\perso\\\oscar_deploie\\\\" /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
+
 exit 0
