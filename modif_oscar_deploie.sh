4e8f1dd620dfe68c0756c77df4c14303a05f315c
domijassin <dominique.jassin@ac-lyon.fr>
2014-03-25 15:58:57 +0100

    Update modif_oscar_deploie.sh

--- modif_oscar_deploie.sh
+++ modif_oscar_deploie.sh
@@ -7,7 +7,9 @@
 #### on fait un test pour voir si le fichier existe
 
 if [ -e /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat ] ; then
-
+if [ -e /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat.old ] ; then
+exit 0
+else
 #### on fait une copie de sauvegarde du fichier source
 cp /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat.old
 
@@ -23,6 +25,7 @@ sed -i "/oui_wpkg/a \if exist \"%programfiles%\\\Wpkg-GP\\\uninstall.exe\" call 
 sed -i "/regedit \/s %windir%\\\oscar_deploie\\\oscar_samba_domaine.reg/a \copy \\\\\\\%ip_scribe%\\\admin\\\perso\\\oscar_deploie\\\oscar_var.txt \\\\\\\%ip_scribe%\\\sysprep\\\perso\\\oscar_deploie\\\\" /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
 
 exit 0
+fi
 
 else
 
