2664a8a901e710e58b54246116845ef207d8d9d6
domijassin <dominique.jassin@ac-lyon.fr>
2014-03-25 11:12:38 +0100

    Update modif_oscar_deploie.sh

--- modif_oscar_deploie.sh
+++ modif_oscar_deploie.sh
@@ -4,7 +4,11 @@
 
 #### on fait un test pour voir si le fichier existe
 
+
+
 if [ -e /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat ] ; then
+cp /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat /home/a/admin/oscar_deploie/oscar_deploie_numero_poste.bat.old
+
 sed -i.BAK "/%programfiles%/a \if exist \"%programfiles%\\\Wpkg-GP\" echo oui  > %windir%\\\oscar_deploie\\\oui_wpkg" /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
 
 sed -i.BAK "/oui_wpkg/a \if exist \"%programfiles%\\\Wpkg-GP\\\uninstall.exe\" call \"%programfiles%\\\Wpkg-GP\\\uninstall.exe\" \/S" /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
