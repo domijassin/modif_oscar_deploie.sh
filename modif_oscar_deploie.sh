64bd5a640b22180fc13c5a5cce4c8cfceb13edcf
domijassin <dominique.jassin@ac-lyon.fr>
2014-03-25 12:02:55 +0100

    Update modif_oscar_deploie.sh

--- modif_oscar_deploie.sh
+++ modif_oscar_deploie.sh
@@ -9,11 +9,10 @@
 if [ -e /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat ] ; then
 cp /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat /home/a/admin/oscar_deploie/oscar_deploie_numero_poste.bat.old
 
-sed -i "/%programfiles%/a \if exist \"%programfiles%\\\Wpkg-GP\" echo oui  > %windir%\\\oscar_deploie\\\oui_wpkg" /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
+ sed -i "s/Wpkg-GP\\\uninstall.exe\" \"%programfiles%\\\Wpkg-GP\\\uninstall.exe\" \/S/Wpkg-GP\\\" echo oui > %windir%\\\oscar_deploie\\\oui_wpkg/"/home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
 
 sed -i "/oui_wpkg/a \if exist \"%programfiles%\\\Wpkg-GP\\\uninstall.exe\" call \"%programfiles%\\\Wpkg-GP\\\uninstall.exe\" \/S" /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
 
-sed -i "s/%programfiles%/ " /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
 
 sed -i "/regedit \/s %windir%\\\oscar_deploie\\\oscar_samba_domaine.reg/a \copy \\\\\\\%ip_scribe%\\\admin\\\perso\\\oscar_deploie\\\oscar_var.txt \\\\\\\%ip_scribe%\\\sysprep\\\perso\\\oscar_deploie\\\\" /home/a/admin/perso/oscar_deploie/oscar_deploie_numero_poste.bat
 
